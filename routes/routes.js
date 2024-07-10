aa9d7f5e47caba3b8bb7a51d1496553e8451bfed
BrajanS <szczapbrajan33@gmail.com>
2024-07-10 09:38:55 +0200

    1.01

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 require("dotenv").config({ path: "config.env" });
 const axios = require("axios");
 
+
 route.get("/", async (req, res) => {
   res.render("index.ejs", {
     meteo: null,
@@ -14,7 +15,7 @@ route.get("/meteo", async (req, res) => {
   const apiKey = process.env.meteo_api;
   const ville = req.query.city;
   const meteoURL = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&units=metric&appid=${apiKey}`;
-
+  
   try {
     const response = await axios.get(meteoURL);
     const meteo = response.data;
